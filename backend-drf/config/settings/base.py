ee1cd6fab99219a156a42cc7f472f100f70cabe2
aho <matsue4692@gmail.com>
2024-03-31 23:50:51 +0900

    Rename: movie -> video app名をmovieからvideoに変更

--- backend-drf/config/settings/base.py
+++ backend-drf/config/settings/base.py
@@ -117,7 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-AUTH_USER_MODEL = 'api.video.User'
+AUTH_USER_MODEL = 'video.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/

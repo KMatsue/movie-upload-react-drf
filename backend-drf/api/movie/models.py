51058475c44852666d7c3b505b7ffdc3b85f8fa0
aho <matsue4692@gmail.com>
2024-03-31 22:03:34 +0900

    Update: models.py カスタムのユーザーモデルを記述

--- backend-drf/api/movie/models.py
+++ backend-drf/api/movie/models.py
@@ -1,3 +1,51 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+
+# 参考ドキュメント:https://docs.djangoproject.com/ja/5.0/topics/auth/customizing/#a-full-example
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Email address is must')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email, and password.
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, editable=False)
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email

eeabe8407a4100bc6a99a9ea95b5d4ef96435dd0
aho <matsue4692@gmail.com>
2024-04-02 13:56:42 +0900

    Update: views.py

--- backend-drf/api/video/views.py
+++ backend-drf/api/video/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Video
+from .serializers import VideoSerializer, UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer

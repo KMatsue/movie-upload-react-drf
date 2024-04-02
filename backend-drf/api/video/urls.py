691193f15549dbbf7fa6680eb1da9fc67de6c99f
aho <matsue4692@gmail.com>
2024-04-02 16:14:55 +0900

    Update: urls.py

--- backend-drf/api/video/urls.py
+++ backend-drf/api/video/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 
+from .views import CreateUserView, VideoViewSet
+
 router = routers.DefaultRouter()
+router.register('videos', VideoViewSet)
 
 urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
     path('', include(router.urls)),
 ]

26550db00aeb9aaec8c70a0b00ef796f34d1f102
aho <matsue4692@gmail.com>
2024-04-03 09:27:40 +0900

    Update: tailwind.config.js MaterialTailwind設定

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Button } from "@material-tailwind/react";
 
 function App() {
   return (
@@ -8,6 +9,16 @@ function App() {
       </button>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1 className="mt-10  underline underline-offset-4">Hello world!</h1>
+      <div className="flex  mx-auto w-max gap-4 mt-8 ">
+        <Button>Button</Button>
+        <Button color="green" className="text-xl text-yellow-500">
+          Button
+        </Button>
+        <Button variant="filled">filled</Button>
+        <Button variant="gradient">gradient</Button>
+        <Button variant="outlined">outlined</Button>
+        <Button variant="text">text</Button>
+      </div>
     </>
   );
 }

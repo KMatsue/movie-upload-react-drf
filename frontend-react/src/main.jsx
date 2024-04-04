b93a212080d8aa9a58ef05433a557ac0f50a761e
aho <matsue4692@gmail.com>
2024-04-04 14:47:43 +0900

    Set: main.jsx ルーティング設定

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import { CookiesProvider } from "react-cookie";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <CookiesProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/video" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </CookiesProvider>
   </React.StrictMode>
 );

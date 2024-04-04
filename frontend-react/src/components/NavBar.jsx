5143fb57fd83fcc86c88934b5354598484055f90
aho <matsue4692@gmail.com>
2024-04-04 15:52:49 +0900

    Update: Login.jsx UI実装

--- frontend-react/src/components/NavBar.jsx
+++ frontend-react/src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 // import React from "react";
 import { FiLogOut } from "react-icons/fi";
 import { RiFolderVideoFill } from "react-icons/ri";
+import { withCookies } from "react-cookie";
+import PropTypes from "prop-types";
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const Logout = () => {
+    props.cookies.remove("jwt-token");
+    window.location.href = "/";
+  };
+
   return (
     <nav className="w-full border-b bg-primeColor border-gray-500 py-2 px-4">
       <div className="max-w-screen-2xl mx-auto flex items-center justify-between">
@@ -10,12 +17,14 @@ const NavBar = () => {
           <RiFolderVideoFill className="text-2xl mr-1" />
           <span className="text-2xl font-bold font-comic">Video-UP</span>
         </button>
-        <button>
+        <button onClick={() => Logout()}>
           <FiLogOut className="text-2xl" />
         </button>
       </div>
     </nav>
   );
 };
-
-export default NavBar;
+NavBar.propTypes = {
+  cookies: PropTypes.object,
+};
+export default withCookies(NavBar);

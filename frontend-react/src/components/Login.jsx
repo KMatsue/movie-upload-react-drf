71ac535744bffd7b50faf48b8e6457f6f3dc9f8e
aho <matsue4692@gmail.com>
2024-04-04 17:59:59 +0900

    Add: CircularProgress.jsx

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useReducer } from "react";
-import { withCookies } from "react-cookie";
+// import { withCookies } from "react-cookie";
 import {
   START_FETCH,
   FETCH_SUCCESS,
@@ -7,6 +7,8 @@ import {
   INPUT_EDIT,
   TOGGLE_MODE,
 } from "./actionTypes";
+import CircularProgress from "./CircularProgress";
+import { FaLock } from "react-icons/fa6";
 
 const initialState = {
   isLoading: false,
@@ -63,22 +65,31 @@ const loginReducer = (state, action) => {
 
 const Login = () => {
   const [state, dispatch] = useReducer(loginReducer, initialState);
+  const inputChangedLog = () => (event) => {
+    //const cred = state.credentialsLog;
+    //cred[event.target.name] = event.target.value;
+    dispatch({
+      type: INPUT_EDIT,
+      // inputName: "state.credentialLog",
+      //payload: cred,
+      inputName: event.target.name,
+      payload: event.target.value,
+    });
+  };
 
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      {state.isLoading && <CircularProgress />}
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=orange&shade=600"
-          alt="Your Company"
-        />
+        <FaLock color="orange" className="mx-auto h-10 w-auto " />
+
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" onSubmit={() => {}}>
           <div>
             <label
               htmlFor="email"
@@ -91,6 +102,8 @@ const Login = () => {
                 id="email"
                 name="email"
                 type="email"
+                value={state.credentialsLog.email}
+                onChange={inputChangedLog()}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
               />
@@ -119,10 +132,17 @@ const Login = () => {
                 id="password"
                 name="password"
                 type="password"
+                value={state.credentialsLog.password}
+                onChange={inputChangedLog()}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primeColor sm:text-sm sm:leading-6"
               />
             </div>
+          </div>
+          <div>
+            <span className="flex text-red-500 font-semibold text-lg">
+              {state.error}
+            </span>
           </div>
 
           <div>

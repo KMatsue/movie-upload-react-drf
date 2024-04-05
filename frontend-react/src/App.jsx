6927fa09888f51f59301a0ca7eb01afe73ec148a
aho <matsue4692@gmail.com>
2024-04-05 23:50:02 +0900

    Add: ApiContext.jsx

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,28 +1,32 @@
 import "./App.css";
 import { Button } from "@material-tailwind/react";
 import NavBar from "./components/NavBar";
+import { ThemeProvider } from "@material-tailwind/react";
+import ApiContextProvider from "./context/ApiContext";
 
 function App() {
   return (
-    <>
-      <main className="text-center">
-        <NavBar />
-        <button className="w-32 h-8 bg-black text-white rounded-md hover:bg-slate-700 duration-300">
-          連絡する
-        </button>
-        <h1 className="font-comic text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <div>
-          <Button variant="filled" className="bg-primeColor">
-            filled
-          </Button>
-          <Button variant="gradient">gradient</Button>
-          <Button variant="outlined">outlined</Button>
-          <Button variant="text">text</Button>
-        </div>
-      </main>
-    </>
+    <ApiContextProvider>
+      <ThemeProvider>
+        <main className="text-center">
+          <NavBar />
+          <button className="w-32 h-8 bg-black text-white rounded-md hover:bg-slate-700 duration-300">
+            連絡する
+          </button>
+          <h1 className="font-comic text-3xl font-bold underline">
+            Hello world!
+          </h1>
+          <div>
+            <Button variant="filled" className="bg-primeColor">
+              filled
+            </Button>
+            <Button variant="gradient">gradient</Button>
+            <Button variant="outlined">outlined</Button>
+            <Button variant="text">text</Button>
+          </div>
+        </main>
+      </ThemeProvider>
+    </ApiContextProvider>
   );
 }
 
--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@material-tailwind/react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Login from "./components/Login.jsx";
 import { CookiesProvider } from "react-cookie";
@@ -10,14 +9,12 @@ import { CookiesProvider } from "react-cookie";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CookiesProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Login />} />
-            <Route exact path="/video" element={<App />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/video" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </CookiesProvider>
   </React.StrictMode>
 );

102500c0f778dc761672247921cd304652399c99
aho <matsue4692@gmail.com>
2024-04-07 01:15:04 +0900

    Update: VideoItem.jsx VideoList.jsx ビデオデータを取得しリストにカード形式で表示

--- frontend-react/src/components/VideoItem.jsx
+++ frontend-react/src/components/VideoItem.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import { ApiContext } from "../context/ApiContext";
 
-const VideoItem = () => {
-  return <div>VideoItem</div>;
+const VideoItem = ({ video }) => {
+  console.log(video.title);
+
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg mb-2 mx-2 bg-white">
+      <img className="w-full" src={video.thumbnail} alt="Sunset" />
+      <div className="px-2 py-2">
+        <div className="text-left font-bold text-lg">{video.title}</div>
+      </div>
+    </div>
+  );
 };
-
+VideoItem.propTypes = {
+  video: PropTypes.object,
+};
 export default VideoItem;
--- frontend-react/src/components/VideoList.jsx
+++ frontend-react/src/components/VideoList.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-
+import { useContext } from "react";
+import { ApiContext } from "../context/ApiContext";
+import VideoItem from "./VideoItem";
 const VideoList = () => {
-  return <div>VideoList</div>;
+  const { videos } = useContext(ApiContext);
+  // console.log(videos);
+  const listOfVideos = videos.map((video) => (
+    <VideoItem key={video.id} video={video} />
+  ));
+
+  return (
+    <div className="grid ">
+      <div className="bg-gray-800">{listOfVideos}</div>
+    </div>
+  );
 };
 
 export default VideoList;

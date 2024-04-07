b05d95e288cffa26548695bf427ec19ac27a0785
aho <matsue4692@gmail.com>
2024-04-07 08:50:09 +0000

    Rename: base.py base.py -> settings.py

--- backend-drf/config/settings/development.py
+++ backend-drf/config/settings/development.py
@@ -1,9 +1,11 @@
-from .base import *
+from .settings import *
+from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }

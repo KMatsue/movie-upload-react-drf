0e0ee27efcffdec274e51e17e04a1cea8c0afe56
aho <matsue4692@gmail.com>
2024-04-11 00:50:36 +0000

    Update: Login.jsx SignUpフォームに会社名の入力フォームを追加

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -142,6 +142,26 @@ const Login = (props) => {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" onSubmit={login}>
+          {!state.isLoginView && (
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Company Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="username"
+                  value={state.credentialsLog.username}
+                  onChange={inputChangedLog()}
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          )}
           <div>
             <label
               htmlFor="email"
